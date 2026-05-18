136a0933986aae326be1571119c0f64d7190431f
Neevsharma <neevsharma412@gmail.com>
2026-05-18 13:13:53 +0530

    Add files via upload

---
+++ SurfaceAreaCube.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class SurfaceAreaCube {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter side: ");
+        double side = sc.nextDouble();
+
+        double area = 6 * side * side;
+
+        System.out.println("Surface Area = " + area);
+    }
+}
