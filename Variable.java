7bf5d7a191b7840b648d9d55087e7398444588e9
Neevsharma <neevsharma412@gmail.com>
2026-05-18 13:09:55 +0530

    Add files via upload

--- Variable.java
+++ Variable.java
@@ -1,10 +1,10 @@
-public class Variable{
+public class Variable {
     public static void main(String[] args) {
 
         int number = 10;          
         double price = 99.5;      
-        char grade = 'A';       
-        String name = "Neev";   
+        char grade = 'A';         
+        String name = "Aditya";   
 
         System.out.println("Number: " + number);
         System.out.println("Price: " + price);
@@ -12,5 +12,3 @@ public class Variable{
         System.out.println("Name: " + name);
     }
 }
-    
-
