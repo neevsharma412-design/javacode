99921f0b415d20cf84fef38989ab2ab395b2a53c
Neevsharma <neevsharma412@gmail.com>
2026-05-18 13:23:36 +0530

    Add files via upload

---
+++ Student.java
@@ -0,0 +1,22 @@
+public class Student {
+    int id;
+    String name;
+
+
+    Student(int i, String n) {
+        id = i;
+        name = n;
+    }
+
+    
+    void display() {
+        System.out.println(id + " " + name);
+    }
+
+    public static void main(String[] args) {
+
+        Student s1 = new Student(101, "Aditya");
+
+        s1.display();
+    }
+} 
